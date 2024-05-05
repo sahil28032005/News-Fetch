b9e0009e667af2d498af94cd02e23cdfa8908b26
$@#il <sahilsadekar249775@gmail.com>
2024-05-06 01:32:52 +0530

    added pervious and next news fecth buttons

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './newsItem.css'
+import './newsItem.css';
 
 class Item extends React.Component {
     constructor(props) {
@@ -7,17 +7,23 @@ class Item extends React.Component {
         this.state = {
             data: null,
             isLoading: false,
-            error: null
+            error: null,
+            page: 1
         };
         console.log("constructor of newsitem");
-
+        //binding this permission to async functions
+        this.next = this.next.bind(this);
+        this.prev = this.prev.bind(this);
 
     }
-    componentDidMount() {
-        const apikey=process.env.REACT_APP_API_KEY;
-        var url = 'https://newsapi.org/v2/top-headlines?' +
-            'country=us&'+apikey;
+    //inaitalizor
+    initializeComponentData = () => {
+        this.setState({ isLoading: true });
+        const apikey = process.env.REACT_APP_API_KEY;
+        var url = `https://newsapi.org/v2/top-headlines?` +
+            `country=us&'+'&page=${this.state.page}&` + 'pageSize=8&' + apikey;
         var req = new Request(url);
+        console.log(this.state.isLoading);
         fetch(req)
             .then(response => response.json())
             .then(data => {
@@ -25,24 +31,64 @@ class Item extends React.Component {
             })
 
     }
+
+    //this will run after rendering of component at first render
+    componentDidMount() {
+        this.initializeComponentData();
+    }
+    next = async function (req, res) {
+        console.log("next");
+        this.setState({ page: this.state.page + 1 }, () => {
+            this.initializeComponentData(); // Call after state is updated
+        });
+    }
+
+    prev = async function (req, res) {
+        console.log("previous");
+        this.setState({ page: this.state.page - 1 }, () => {
+            this.initializeComponentData(); // Call after state is updated
+        });
+    }
     render() {
-        console.log(this.state.data)
+        // console.log(this.state.data)
         return (
-            <div className='containerDiv'>{
-                this.state.data && this.state.data.articles.map((article) =>
-                <div className="card" style={{width:"18rem",height:"30rem",margin:"10px"} }>
-                    <img style={{height:"10rem"}} src={article.urlToImage} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{article.title}</h5>
-                        <p className="card-text">{article.description}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+            <>
+                {this.state.isLoading && (
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div className="spinner-border text-success" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                )}
+
+
+
+                <div className='containerDiv'>{
+                    this.state.data && this.state.data.articles.map((article) =>
+                        <div className="card" style={{ width: "18rem", height: "30rem", margin: "10px" }}>
+                            <img style={{ height: "10rem" }} src={article.urlToImage} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{article.title}</h5>
+                                <p className="card-text">{article.description}</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    )
+
+                }
+
+                </div>
+                <div style={{ padding: "30px" }}>
+                    <div className="d-flex justify-content-between">
+                        <button disabled={this.state.page<=1} onClick={this.prev} className="btn btn-primary me-2">Prev</button>
+                        <div className="flex-grow-1"></div>
+                        <button onClick={this.next} className="btn btn-primary ms-2">Next</button>
                     </div>
                 </div>
-                )
-                
-            }
 
-            </div>
+                {console.log("at end" + this.state.isLoading)}
+            </>
+
         );
     }
 }
